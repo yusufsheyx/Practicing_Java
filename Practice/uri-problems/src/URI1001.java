b54a6d48788b6ef2819248fb0843b87d7507b17e
yusufsheyx <yusufmuhammadov102@gmail.com>
2022-06-18 12:25:36 +0500

    Finish URI1001

---
+++ Practice/uri-problems/src/URI1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class URI1001 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        int answer = a + b;
+        System.out.println("X = " + answer);
+    }
+}
